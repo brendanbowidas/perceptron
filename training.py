91b89ff1ca4e3ccef1f40970274fcd7dac5666ef
Brendan Bowidas <brendan@Steven-Brien.local>
2016-03-23 12:55:54 -0400

    initial commit

---
+++ training.py
@@ -0,0 +1,11 @@
+#import perceptron
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+print(df.tail())
+
+y = df.iloc[0:100, 4].values
+
+print(y)
